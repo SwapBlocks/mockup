9d51099252d7d0802858f93ebe369919907c49f9
lance <lancekrogers@gmail.com>
2018-10-11 02:28:33 -0400

    added UAI to asset ID

--- swap_mock/interest/admin.py
+++ swap_mock/interest/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from .models import Asset, Transaction, OrderBook, InterestRateSwap
+from .models import Asset, Transaction, OrderBook
 # Register your models here.
 
-# admin.site.register(Asset)
+admin.site.register(Asset)
 admin.site.register(Transaction)
 admin.site.register(OrderBook)
-admin.site.register(InterestRateSwap)
--- swap_mock/interest/models.py
+++ swap_mock/interest/models.py
@@ -40,18 +40,6 @@ class Asset(PolymorphicModel):
     def __str__(self):
         return f"AssetId: {self.UAI}"
 
-    # class Meta:
-        # abstract = True
-
-
-class InterestRateSwap(Asset):
-    # Interest Rate Forms Fields
-    assetType = models.CharField(default="interstRateSwap", max_length=100)
-    description = models.TextField()
-    interestRate = models.DecimalField(decimal_places=10, max_digits=20)
-    floating = models.BooleanField()
-    basis = models.CharField(choices=BENCHMARKS, max_length=100, blank=True)
-
 
 class Transaction(models.Model):
     transactionType = models.CharField(choices=TRANSACTION_TYPES,
@@ -140,7 +128,7 @@ def asset_post_save(sender, instance, created=False, **kwargs):
 
             # Concatenate byte strings dp1 + stnd, hash it and convert to hex
             uai_bytes = hashlib.sha3_256(packet_hash + standard_data).digest()
-            UAI = f"{instance.weight}{hexlify(uai_bytes).decode()}"
+            UAI = f"UAI{instance.weight}{hexlify(uai_bytes).decode()}"
 
             # Set and save
             instance.UAI = UAI
@@ -164,87 +152,6 @@ def asset_post_save(sender, instance, created=False, **kwargs):
                   """)
         except Exception as e:
            # Do other stuff
-            print(e)
-
-
-@receiver(post_save, sender=InterestRateSwap)
-def irswap_post_save(sender, instance, created=False, **kwargs):
-    """
-    UAI and registrar setting
-    """
-    if created:
-        try:
-            # Set registrar to users public address
-            instance.registrar = PUBLIC_ADDRESS
-            instance.save()
-
-            # serialize object and extract fields needed for hashing
-            from pprint import pprint
-            pprint(dir(instance))
-            serial_asset = serializers.serialize('json', [instance, ])
-            json_asset = json.loads(serial_asset)
-            print("------------------------------------")
-            pprint(json_asset)
-            needed_fields = json_asset[0]['fields']
-            consortium_keys = ['assetType', 'description', 'basis', 'floating']
-            oth_keys = ['amount', 'consortium',
-                        'registrar', 'weight', 'validThrough']
-
-            # create 2 dicts 1 of data_packet1 and one for standard data
-            # Convert each dict into encoded json
-            consortium_data = needed_fields['consortiumPacket']
-
-            municipal_data = needed_fields['municipalPacket']
-            standard_data = json.dumps({key: needed_fields[key]
-                             for key in oth_keys}, sort_keys=True).encode()
-
-            # hash each byte string sha3_256
-            if municipal_data:
-                encoded_municipal_data = municipal_data.encode()
-                municipal_packet_hash = hashlib.sha3_256(encoded_municipal_data
-                                                      ).digest()
-            else:
-                municipal_packet_hash = b''
-            if consortium_data:
-                encoded_consortium_data = consortium_data.encode()
-                consortium_packet_hash = hashlib.sha3_256(
-                    encoded_consortium_data).digest().decode()
-            else:
-                consortium_packet_hash = b''
-            packet_to_hash = municipal_packet_hash + consortium_packet_hash
-            if packet_to_hash:
-                packet_hash = hashlib.sha3_256(packet_to_hash).digest()
-            else:
-                packet_hash = b''
-            standard_data = hashlib.sha3_256(standard_data).digest()
-
-            # Concatenate byte strings dp1 + stnd, hash it and convert to hex
-            uai_bytes = hashlib.sha3_256(packet_hash + standard_data).digest()
-            UAI = f"{instance.weight}{hexlify(uai_bytes).decode()}"
-
-            # Set and save
-            instance.UAI = UAI
-            instance.save()
-
-            # Now create a new asset genesis transaction
-            Transaction.objects.create(
-                amount=instance.amount,
-                transactionType="assetGenesis",
-                standardAsset=instance,
-                receipeint=PUBLIC_ADDRESS,
-                consortiumPacket=consortium_data,
-                consortiumPacketHash=consortium_packet_hash.hex(),
-                municipalPacket=municipal_data,
-                municipalPacketHash=municipal_packet_hash.hex()
-            )
-            # Broadcast transaction to the network
-            print(f"""
-                  Transaction is being broadcast to the network
-                  for asset {UAI}
-                  """)
-        except Exception as e:
-            # Do other stuff
-            print('error')
             print(e)
 
 
